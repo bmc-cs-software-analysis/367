5ab0d4947466e16fd87221c21afc7cc0c3223e73
elizabethdinella <elizabeth.a.dinella@gmail.com>
2026-08-31 14:16:46 -0400

    HW6 - AFL

--- hws/hw06/PriorityQueue.java
+++
@@ -1,6 +0,0 @@
-public interface PriorityQueue<E extends Comparable<E>> extends BinaryTree<E>{
-    // returns the element with the minkey, or null when PQ is empty
-    E peek();
-    // removes and returns the element with the minkey, or null when PQ is empty
-    E poll();
-}
